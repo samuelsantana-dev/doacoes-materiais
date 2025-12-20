d2d3ea55f57e63b7d303023a573ef349d61e264d
samuelsantana-dev <samuelsantanadasilva8@gmail.com>
2025-12-19 21:47:37 -0300

    Material ui

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,14 @@
 import './App.css'
+import Button from '@mui/material/Button';
 
 function App() {
 
   return (
     <>
       <h1>Doações de Materiais</h1>
+      <Button variant="text">teste</Button>
+      <Button variant="contained">Contained</Button>
+      <Button variant="outlined">Outlined</Button>
     </>
   )
 }
