01acba31c20cc8487096d2d0906254334028a60a
PS Solutions Tecnology <92067743+PauloTIgit@users.noreply.github.com>
2025-12-23 16:46:20 -0300

    :sparkles: feat: Configurar esLint no projeto #13

--- backend/eslint.config.mjs
+++ backend/eslint.config.mjs
@@ -6,7 +6,12 @@ import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
   {
-    ignores: ['eslint.config.mjs'],
+    ignores: [
+      'eslint.config.mjs',
+      'dist/**',
+      'node_modules/**',
+      '**/*.config.js',
+    ],
   },
   eslint.configs.recommended,
   ...tseslint.configs.recommendedTypeChecked,
